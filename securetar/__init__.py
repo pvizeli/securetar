e3d3e5cdae943574956ccf25583dfd156ee3c16b
J. Nick Koston <nick@koston.org>
2024-02-10 13:14:34 -0600

    fix

--- securetar/__init__.py
+++ securetar/__init__.py
@@ -5,7 +5,7 @@ import os
 import tarfile
 from contextlib import contextmanager
 from pathlib import Path, PurePath
-from typing import IO, Any, Generator, Optional
+from typing import IO, Generator, Optional
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
